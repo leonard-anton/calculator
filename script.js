718da2ed91965bf2e00a7581c301220ed2c665cb
Leonard Anton <124513400+leonard-anton@users.noreply.github.com>
2025-10-25 15:36:55 +0300

    Add operator functions

---
+++ script.js
@@ -0,0 +1,38 @@
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(a, b, operator) {
+
+    a = parseFloat(a);
+    b = parseFloat(b);
+
+    switch (operator) {
+        case '+':
+            return add(a, b);
+            break;
+        case '-':
+            return subtract(a, b);
+            break;
+        case 'x':
+            return multiply(a, b);
+            break;
+        case '/':
+            return divide(a, b);
+            break;
+        default:
+            return null;
+    }
+}
